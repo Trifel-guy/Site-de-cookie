6c190c58e92cd87e727aa72c7e67eb8dd3e94f94
trifel-guy <trifelguy2@gmail.com>
2021-12-22 01:34:12 +0100

    ajout du component homeComponent pour la page d'accueil, css du menu version 1

--- cookieGest/src/app/app.module.ts
+++ cookieGest/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './structure/menu/menu.component';
 import { FooterComponent } from './structure/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
