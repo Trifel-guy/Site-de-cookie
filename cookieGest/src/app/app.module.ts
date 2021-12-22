c9f605d6eeb5347c1e37ff9f11e66fccea9e8a7a
trifel-guy <trifelguy2@gmail.com>
2021-12-22 01:06:08 +0100

    Premier commit après commande ng new cookieGest et generation de 2 components: menu et footer

---
+++ cookieGest/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './src/app/structure/menu/menu.component';
+import { FooterComponent } from './structure/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
