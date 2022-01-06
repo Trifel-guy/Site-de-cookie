a8c88081b7ce0fb9e2f8c8c35aea8c9976e4ea11
trifel-guy <trifelguy2@gmail.com>
2022-01-06 17:22:54 +0100

    creation des composants de base et mise en place des routes avec les routerLink du menu

--- cookieGest/src/app/app-routing.module.ts
+++ cookieGest/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component';
+import { ConnectionComponent } from './pages/connection/connection.component';
+import { CookieComponent } from './pages/cookie/cookie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'cart', component:CartComponent},
+  {path:'connection', component:ConnectionComponent},
+  {path:'cookie', component:CookieComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cookieGest/src/app/app.module.ts
+++ cookieGest/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './structure/menu/menu.component';
 import { FooterComponent } from './structure/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ConnectionComponent } from './pages/connection/connection.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CookieComponent } from './pages/cookie/cookie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ConnectionComponent,
+    CartComponent,
+    CookieComponent
   ],
   imports: [
     BrowserModule,
