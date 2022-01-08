7e81263dc13bdc109e0c27f833f980d56696900d
trifel-guy <trifelguy2@gmail.com>
2022-01-08 02:51:20 +0100

    Deuxième jet design page d'accueil

--- cookieGest/src/app/pages/cart/cart.component.ts
+++ cookieGest/src/app/pages/cart/cart.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  private quantity:number = 0;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public increaseQuantity(): void {
+    this.quantity += 1;
+  }
+
+
+  public getQuantity(): number {
+    return this.quantity;
   }
 
 }
